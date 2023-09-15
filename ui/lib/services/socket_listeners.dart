282cbea5d983c0a12551d283431f6fcddea95504
Vince Liem <vince.liem@iodigital.com>
2023-09-15 14:28:23 +0200

    changed listeners

--- ui/lib/services/socket_listeners.dart
+++ ui/lib/services/socket_listeners.dart
@@ -3,8 +3,14 @@ import 'socket_service.dart';
 class SocketListeners {
   final SocketService _socketService = SocketService();
 
-  void onUserConnected(Function callback) {
-    _socketService.on('user-connected', (data) {
+  void onNewSystemMessage(Function(Map<String, dynamic>) callback) {
+    _socketService.on('new-system-message', (data) {
+      callback(data);
+    });
+  }
+
+  void onNewAssistantMessage(Function(Map<String, dynamic>) callback) {
+    _socketService.on('new-assistant-message', (data) {
       callback(data);
     });
   }
