ba083ed905872172e580daa01aa27b9af509e074
Vince Liem <vince.liem@iodigital.com>
2023-09-15 14:26:04 +0200

    removed the check_onboarding emitter

--- ui/lib/services/socket_emitters.dart
+++ ui/lib/services/socket_emitters.dart
@@ -9,10 +9,6 @@ class SocketEmitters {
     });
   }
 
-  void checkOnboarding(Function(Map<String, dynamic>) ackCallback) {
-    _socketService.emitWithAck('check-onboarding', {}, ack: ackCallback);
-  }
-
   void upsertUser(Map<String, dynamic> details, Function(Map<String, dynamic>) ackCallback) {
     _socketService.emitWithAck('upsert-user', details, ack: ackCallback);
   }
