ebf44fbc20ea75a0895db2db357f7f06da69de73
Vince Liem <vince.liem@iodigital.com>
2023-09-26 16:06:15 +0200

    with frontend hasura auth changes

--- ui/lib/services/auth_service.dart
+++ ui/lib/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -6,6 +7,7 @@ class AuthService {
   AuthService._privateConstructor();
 
   static final AuthService _instance = AuthService._privateConstructor();
+  static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   factory AuthService() {
     return _instance;
@@ -23,6 +25,16 @@ class AuthService {
   }
 
   Stream<User?> get authStateChanges {
+    _auth.authStateChanges().listen((User? user) {
+      if (user != null) {
+        // Listen for metadata changes to force a token refresh
+        _firestore.collection('metadata').doc(user.uid).snapshots().listen((snapshot) {
+          if (snapshot.exists && snapshot.data() != null && snapshot.data()!['refreshTime'] != null) {
+            user.getIdToken(true); // Force refresh the token
+          }
+        });
+      }
+    });
     return _auth.authStateChanges();
   }
 
@@ -41,6 +53,9 @@ class AuthService {
         );
         final UserCredential authResult = await _auth.signInWithCredential(credential);
         final User? user = authResult.user;
+        if (user != null) {
+          await user.getIdToken(true);
+        }
         return user;
       }
     } catch (error) {
